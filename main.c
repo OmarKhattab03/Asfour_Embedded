fea764813102753d62b6f08edf26165c42fce626
Omarkhattab03 <omarkhattab423@gmail.com>
2022-08-13 11:55:58 +0200

    Sequence

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+# define Max_Size 10
+# define NUM 3
+
+int main()
+{
+    while (1)
+    {
+        int streak=0,temp=0,cnt=0,Max=0,occ=0;
+        int arr[Max_Size];
+        printf("Please Enter the array:\n");
+
+
+        for (cnt=0;cnt<Max_Size;cnt++)
+        {
+            scanf("%d",&arr[cnt]);
+        }
+
+
+        for (cnt=0;cnt<=Max_Size;cnt++)
+        {
+            if ((arr[cnt]==NUM) && (cnt<=Max_Size))
+            {
+            occ++;
+            }
+            else if (occ>Max)
+            {
+            Max=occ;
+            occ=0;
+            }
+        }
+        printf("Maximum occurrence=%d",Max);
+        printf("\n\n\n");
+    }
+}
